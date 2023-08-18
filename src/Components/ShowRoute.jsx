52e3b7395dace9e988ef18477a62f274aa820f1b
Javad <1998javad@gmail.com>
2023-08-18 17:23:12 +0200

    update

--- src/Components/ShowRoute.jsx
+++ src/Components/ShowRoute.jsx
@@ -1,20 +1,35 @@
-import { useState } from "react";
+import moment from "moment"
 
 function ShowRoute({info}){
-  console.log(info);
+ 
+  let TransportMedel=[]
+  const format = "HH:mm:ss";
+  const orginTime= info[0].Origin.time
+  const DestinationTime=info[info.length-1].Destination.time
+  const orginTimeMoment = moment(orginTime, format);
+  const DestinationTimeMoment = moment(DestinationTime, format);
+  const timeDifference = DestinationTimeMoment.diff(orginTimeMoment);
+  const duration = moment.duration(timeDifference);
+  let formattedDuration = ` ( ${duration.hours()}H , ${duration.minutes()}Min )`
+  if(duration.hours()=== 0){
+     formattedDuration = ` ( ${duration.minutes()}Min )`
+
+
+  }
+ 
 
   
-    const content =info.map((route)=>{
-      console.log(route);
-      return <div className="border-black border-2 py-2 m-2 ">
-        <div><p>{route.Origin.name}&nbsp;-&nbsp;{route.Destination.name} </p>  <p>{route.Origin.time}&nbsp;-&nbsp;{route.Destination.time} </p></div>
-        <div>
-          <p>{route.name} </p>
-        </div>
-      </div>
+    info.map((route)=>{
+      TransportMedel.push(route.name)
+      
 
     })
   
-  return <div>{content}</div>
+  return <div className="border-black border-2 py-2 m-2 ">
+  <div><p className="font-bold">{info[0].Origin.name}&nbsp;-&nbsp;{info[info.length-1].Destination.name} </p>  <p>{info[0].Origin.time}&nbsp;-&nbsp;{info[info.length-1].Destination.time} {formattedDuration} </p></div>
+  <div>
+    <p>{TransportMedel.toString()} </p>
+  </div>
+</div>
 }
 export default ShowRoute
