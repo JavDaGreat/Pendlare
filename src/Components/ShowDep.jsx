4d403d36d3a71647df5bc5b995ade6745f273d20
Javad <1998javad@gmail.com>
2024-02-17 14:57:18 +0100

    Refactor ShowDep and NearbyPage components

--- src/Components/ShowDep.jsx
+++ src/Components/ShowDep.jsx
@@ -1,32 +1,35 @@
-import { useState } from "react"
-import moment from "moment"
+import { useState } from "react";
+import moment from "moment";
 
-function ShowDep({name,time,direction}){
-  
-  let UserTime=new Date().toLocaleTimeString()
-  let BusDepartureTime=time
+function ShowDep({ name, time, direction }) {
+  let UserTime = new Date().toLocaleTimeString();
+  let BusDepartureTime = time;
   const format = "HH:mm:ss";
   const UserTimeMoment = moment(UserTime, format);
   const BusDepartureTimeMoment = moment(BusDepartureTime, format);
   const timeDifference = BusDepartureTimeMoment.diff(UserTimeMoment);
   const duration = moment.duration(timeDifference);
-  let formattedDuration = duration.minutes()
-  if ( isNaN(formattedDuration) ){
-   formattedDuration = "mer än 1 timme "
-  } if(formattedDuration<= 1){
-    formattedDuration ="nu"
+  let formattedDuration = duration.minutes();
+  if (isNaN(formattedDuration)) {
+    formattedDuration = "mer än 1 timme ";
+  }
+  if (formattedDuration <= 1) {
+    formattedDuration = "nu";
   }
 
-  if(typeof formattedDuration === "number"){
-    formattedDuration = `  ${formattedDuration} minuter `
+  if (typeof formattedDuration === "number") {
+    formattedDuration = `  ${formattedDuration} minuter `;
   }
+  const busNr = name.replace(/\D/g, "");
+  const addLine = "Linje " + busNr;
+  const directionWithoutParentheses = direction.replace(/\(.*?\)/g, "");
 
-  return <div className="py-4 px-4 bg-gray-300 ">
-    <h1>{name}</h1>
-    <p> Mot : {direction}</p>
-    <p className="font-bold">  {formattedDuration} </p>
-
-
-  </div>
+  return (
+    <div className="py-4 px-4 bg-gray-300 ">
+      <h1>{addLine}</h1>
+      <p> Mot : {directionWithoutParentheses}</p>
+      <p className="font-bold"> {formattedDuration} </p>
+    </div>
+  );
 }
-export default ShowDep
+export default ShowDep;
--- src/Pages/NearbyPage.jsx
+++ src/Pages/NearbyPage.jsx
@@ -1,57 +1,71 @@
-import { useEffect } from "react"
-import { nearbyStop } from "../Components/functions"
-import { useState } from "react"
-import ShowStop from "../Components/ShowStop"
-import PacmanLoader from "react-spinners/PacmanLoader"
-import { useNavigate } from "react-router-dom"
+import { useEffect } from "react";
+import { nearbyStop } from "../Components/functions";
+import { useState } from "react";
+import ShowStop from "../Components/ShowStop";
+import PacmanLoader from "react-spinners/PacmanLoader";
+import { useNavigate } from "react-router-dom";
 
-
-function NearbyPage(){
-  const [stops,setStops]=useState()
-  const[loading,setLoading]=useState(false)
+function NearbyPage() {
+  const [stops, setStops] = useState();
+  const [loading, setLoading] = useState(false);
   const override = {
     display: "block",
     margin: "0 auto",
     borderColor: "red",
   };
-  const navigate=useNavigate()
+  const navigate = useNavigate();
 
-  async  function getStops(){
-    setLoading(true)
-    let {stopLocationOrCoordLocation} = await nearbyStop()
-    setStops(stopLocationOrCoordLocation)
-    setLoading(false)
-   
-
-    
-
+  async function getStops() {
+    setLoading(true);
+    let { stopLocationOrCoordLocation } = await nearbyStop();
+    setStops(stopLocationOrCoordLocation);
+    setLoading(false);
   }
 
-  useEffect(()=>{
-    getStops()
+  useEffect(() => {
+    getStops();
+  }, []);
+  const content = stops?.map((stop) => {
+    return (
+      <ShowStop
+        dist={stop.StopLocation.dist}
+        name={stop.StopLocation.name}
+        key={stop.StopLocation.extId}
+        id={stop.StopLocation.extId}
+      />
+    );
+  });
+  return (
+    <div>
+      {loading ? (
+        <div className="flex justify-center items-center h-screen w-screen ">
+          <PacmanLoader
+            color={"#1212"}
+            loading={loading}
+            cssOverride={override}
+            size={50}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : (
+        <div>
+          {" "}
+          <button
+            onClick={() => {
+              navigate("/");
+            }}
+            className="rounded bg-black py-2 px-4 text-white mx-2 my-2">
+            back
+          </button>{" "}
+          <h1 className=" text-center font-bold text-lg">Välj en hållplats</h1>{" "}
+        </div>
+      )}
 
-
-
-  },[])
-  const content= stops?.map((stop)=>{
-    return <ShowStop dist={stop.StopLocation.dist} name={stop.StopLocation.name} key={stop.StopLocation.extId} id={stop.StopLocation.extId}/> 
-  })
-   return <div>
-    { loading ?
-       <PacmanLoader
-        color={"#4287f5"}
-        loading={loading}
-        cssOverride={override}
-        size={50}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-        
-      /> : <div> <button onClick={()=>{navigate("/")}} className="rounded bg-black py-2 px-4 text-white mx-2 my-2">back</button> <h1 className=" text-center font-bold text-lg">Välj en hållplats</h1>  </div>}
-   
-    <div className="grid md:grid-cols-3 grid-cols-2 gap-4 py-2 px-2 mx-2">
-      
-      {content}
+      <div className="grid md:grid-cols-3 grid-cols-2 gap-4 py-2 px-2 mx-2">
+        {content}
+      </div>
     </div>
-  </div>
+  );
 }
-export default NearbyPage
+export default NearbyPage;
