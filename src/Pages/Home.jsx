ed0008f624589557664bcaffc8473acc80c7f3ed
Javad <1998javad@gmail.com>
2024-02-17 14:05:46 +0100

    Refactor Home component and fix typos

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,22 +1,33 @@
-import bus from "../assets/bus.png"
-import union from "../assets/Union.png"
-import { useNavigate } from "react-router-dom"
-function Home (){
-  const navigate =useNavigate()
+import bus from "../assets/bus.png";
+import union from "../assets/Union.png";
+import { useNavigate } from "react-router-dom";
+function Home() {
+  const navigate = useNavigate();
 
-
-  return <div className="grid grid-cols-3 gap-4 items-center">
-    <img src={bus} alt="" />
-    <div className="flex justify-center items-center flex-col gap-4">
-      <p className="text-center">Hitta närmaste Hållplatsen </p>
-    <button onClick={()=>{navigate("/nearbystop")}} className="rounded bg-black text-white h-[50px] w-[100px] ">Hitta</button>
-    <p className="text-center">__Eller__</p>
-    <p>Reseplaneran</p>
-    <button onClick={()=>{navigate("/reseplaneran")}} className="rounded bg-black text-white h-[50px] w-[100px] ">Reseplaneran</button>
-
+  return (
+    <div className="grid grid-cols-3 gap-4 items-center">
+      <img src={bus} alt="" />
+      <div className="flex justify-center items-center flex-col gap-4">
+        <p className="text-center">Hitta närmaste Hållplatsen </p>
+        <button
+          onClick={() => {
+            navigate("/nearbystop");
+          }}
+          className="rounded bg-black text-white h-[50px] w-[100px] ">
+          Hitta
+        </button>
+        <p className="text-center">__Eller__</p>
+        <p>Reseplaneraren</p>
+        <button
+          onClick={() => {
+            navigate("/reseplaneran");
+          }}
+          className="rounded bg-black text-white h-[50px] w-[100px] ">
+          Reseplaneraren
+        </button>
+      </div>
+      <img src={union} alt="" />
     </div>
-    <img src={union} alt="" />
-    
-  </div>
+  );
 }
-export default Home
+export default Home;
