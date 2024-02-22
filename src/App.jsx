6cfc10e6c80f014dcdc3aecb683877ef9a080c54
Javad <1998javad@gmail.com>
2024-02-22 18:10:16 +0100

    Refactor import statements and fix formatting in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,34 @@
-import { RouterProvider,createBrowserRouter } from 'react-router-dom'
-import Home from "./Pages/Home"
-import NearbyPage from "./Pages/NearbyPage"
-import Departures from './Pages/Departures'
-import Reseplaneran from './Pages/reseplaneran'
-
-
-
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./Pages/Home";
+import NearbyPage from "./Pages/NearbyPage";
+import Departures from "./Pages/Departures";
+import Reseplaneran from "./Pages/Reseplaneran";
 
 function App() {
   const router = createBrowserRouter([
     {
-      path: '/',
-      element: < Home/>
-    },{
-    path: '/nearbystop',
-    element: <NearbyPage />
-    },{
-      path: '/departures',
-      element: <Departures/>
-      },{
-        path: '/reseplaneran',
-        element: <Reseplaneran/>
-        },
-
-  ])
-
-  
-
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/nearbystop",
+      element: <NearbyPage />,
+    },
+    {
+      path: "/departures",
+      element: <Departures />,
+    },
+    {
+      path: "/reseplaneran",
+      element: <Reseplaneran />,
+    },
+  ]);
 
   return (
-   <div>
-<RouterProvider router={ router } />
-   </div>
-  )
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
-export default App
+export default App;
